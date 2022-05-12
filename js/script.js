c76eaf69177cacce0bf2e1c5a4a0ccafd7e773a0
NaijasonEzeiru <jayseehe1035@gmail.com>
2022-05-12 15:07:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Typing animation
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "UI/UX Designer",
+    "Blockchain Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+const links = document.querySelectorAll(".links");
+const sections = document.querySelectorAll("section");
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove("active"));
+  links[index].classList.add("active");
+}
+
+changeLinkState();
+window.addEventListener("scroll", changeLinkState);
